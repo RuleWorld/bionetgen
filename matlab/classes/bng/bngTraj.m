1d349929037d799c814560e8617914ccf0ea9bd0
unknown <johnsekar@johnsekar-PC.(none)>
2012-11-19 14:03:42 -0500

    Added filterTimepoints method to bngTraj

--- matlab/classes/bng/bngTraj.m
+++ matlab/classes/bng/bngTraj.m
@@ -47,6 +47,18 @@ classdef bngTraj
             
         end
         
+        function obj2 = filterTimepoints(obj1,timepoints1)
+            t_inds = zeros(size(timepoints1));
+            temp = 1;
+            for i=1:1:length(obj1.timepoints)
+                if(any(obj1.timepoints(i)==timepoints1))
+                    t_inds(temp) = i;
+                    temp = temp+1;
+                end
+            end
+            obj2 =  bngTraj(obj1.Labels,obj1.timepoints(t_inds),obj1.val(t_inds,:),obj1.stds(t_inds,:));
+        end 
+        
         
     end
     
@@ -63,32 +75,23 @@ classdef bngTraj
 
         end
         function obj = trajRead_Multiple(filename_array,labels_obj)
-
-
             assert(iscellstr(filename_array),'Provide a list of filenames');
             assert(min(size(filename_array))==1,'Provide a linear list');
-
             num_files = length(filename_array);
-
             traj_obj = bngTraj.trajRead(filename_array{1},labels_obj);
             [r c] = size(traj_obj.val);
-
             traj_3d = zeros(r,c,num_files);
-
             for i=1:1:num_files
                 traj_obj = bngTraj.trajRead(filename_array{i},labels_obj);
                 traj_3d(:,:,i) = traj_obj.val;
             end
-
             m = sum(traj_3d,3)/num_files;
             s = std(traj_3d,0,3);
             t = traj_obj.timepoints;
-            
-            
             obj = bngTraj(labels_obj,t,m,s);
-
-            end
+        end  
     end
+    
     
 end
 
