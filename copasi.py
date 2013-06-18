5cafc13bf632ac32e192ac23ec14c4522e62d332
Jose Juan Tapia <jjtapia@gmail.com>
2013-06-18 19:16:35 -0400

    t
    bugfixing: Corrected a precision error when outputting extremely small quantities. Corrected an equation balancing error where a fraction was being inverted. FIXME: handling of component volume balancing in functional rate laws

--- copasi.py
+++ copasi.py
@@ -170,7 +170,7 @@ def evaluate(fileNumber):
 def compareResults():
     good= 0
     tested = 0
-    for fileNumber in [19]:
+    for fileNumber in range(1,410):
         print fileNumber
         copheaders,copasi = loadResults('copasiBenchmark/output_{0}.txt'.format(fileNumber),'[')
         copheaders = [x.replace(']','').strip() for x in copheaders]
@@ -219,16 +219,16 @@ def compareResults():
         #print 
         #print bNGIndexes,copasiIndexes
         #print np.sum(pow(copasi[:,newCopHeaders] - bng[:,newBngHeaders],2),axis=0)/np.size(copasi,0)
-        atol = copasi[:,newCopHeaders] - bng[:,newBngHeaders]
-        rtol = atol/copasi[:,newCopHeaders]
-        rtol = rtol[np.logical_not(np.isnan(rtol))]
+        #atol = copasi[:,newCopHeaders] - bng[:,newBngHeaders]
+        #rtol = atol/copasi[:,newCopHeaders]
+        #rtol = rtol[np.logical_not(np.isnan(rtol))]
         score =  np.average(pow(np.sum(pow(copasi[:,newCopHeaders] - bng[:,newBngHeaders],2),axis=0)/np.size(copasi,0),0.5))
         
         #mini = np.min(np.sum(pow(copasi[:,newCopHeaders] - bng[:,newBngHeaders],2),axis=0)/np.size(copasi,0))
         #if score>1e-3 and mini<1e-5:
         #    print np.sum(pow(copasi[:,newCopHeaders] - bng[:,newBngHeaders],2),axis=0)/np.size(copasi,0)       
         tested += 1        
-        if score < 0.1:
+        if score < 0.001:
             good += 1
         else:
             print score,np.size(bng,1)
@@ -238,11 +238,10 @@ def compareResults():
     #print copasi[0:3,newCopHeaders]
 #    print bng[:,newBngHeaders]
 #    print copasi[:,newCopHeaders]
-    
+    #print copheaders[1:]
     print tested,good 
     plotResults(bng[:,newBngHeaders],copasi[:,newCopHeaders])           
-    #print bng[0:3,copasiIndexes]
-    #print copasi[0:3,copasiIndexes]
+
         
 def main():
     #xml2cps()
