993610ad2235089f4d26395891c3e3605b4e16d9
cmcarcil <cmcarcil@192.168.0.8>
2014-01-20 16:03:14 -0500

    set's up flow and vascular to extravascular exchange... I don't know how to sort the data. i.e. putting all the parameter terms together after it writes the whole model_file

--- flow/concept/BNGflow.pl
+++ flow/concept/BNGflow.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 my $file = shift @ARGV;
+my $term = "output";
 open(my $filehandle,"<", $file) or die "Problem opening file";
 while ( my $line = <$filehandle> )
 {
@@ -18,16 +19,34 @@ while ( my $line = <$filehandle> )
             my ($key,$val) = $line =~ /\s*(\w+)\s+\"(.+)\"/;
             if ($key eq "file")
             {
-                open(my $modelfile,"<", $val);
-                
-                while ( my $model_line = <$modelfile>)
-                {
-                   print $model_line ;
-                 
-                    
-                }
-                close $modelfile;
+                open(my $modelfile,"<", $val); 
+                while(my $model_line = <$modelfile>)
+               
+				{print $model_line;}	 
+				close $modelfile;}
+    
+
+				if ( $line =~ /^    output/ )
+				{
+					my ($flow) = $line =~ /^    output \"(.+)\"/;
+					my ($rate) = $line =~ /  \"(.+)\"/;
+					print "A\@Vascular_$organ_name -> A\@Vascular_$flow \n"
+	
+
+				}
+            
+            if ( $line =~ /^    exchange/ )
+				{
+					my ($exchange_rate) = $line =~ /^    exchange \"(.+)\"/;
+				
+					print "A\@Vascular_$organ_name <-> A\@Extravascular_$organ_name $exchange_rate \n"
+	
+
+				
             }
-        }
+            }
+            print "end reaction rules\nend model\n";
 }
-    }
+ 
+}
+
