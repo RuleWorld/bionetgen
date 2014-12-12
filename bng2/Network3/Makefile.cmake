9dd241e2285cdbb6d43032c2f9f149529b34b6cf
Leonard A. Harris <lh64@cornell.edu>
2014-12-12 00:05:14 -0600

    Small change to mathutils/Makefile to build the library in the source directory and not copy the header file to an include directory. Moving the library and copying the header file are now done in the Makefile one level up. Also made changes to the CMake build system. This version now works with distributions generated by make_dist.pl. Will now work on getting it to work with the repository code as well.
    Former-commit-id: 67e75040247b1eb2bf547e587088b85fc2f718c9

--- bng2/Network3/Makefile.cmake
+++ bng2/Network3/Makefile.cmake
@@ -22,15 +22,13 @@ NETWORK_BINDIR = bin
 BNG_BINDIR = ../bin
 
 # library prefixes
-MATHUTILS = Mathutils
+MATHUTILS = src/util/mathutils
 CVODE = cvode-2.6.0
-#GSL = gsl-1.9
 MUPARSER = muparser_v2_2_3
 
 # library files
 MATHUTILS_LIB = ${LIBDIR}/libmathutils.a 
 CVODE_LIB = ${LIBDIR}/libsundials_cvode.a ${LIBDIR}/libsundials_nvecserial.a
-#GSL_LIB = ${LIBDIR}/libgsl.a ${LIBDIR}/libgslcblas.a
 MUPARSER_LIB = ${LIBDIR}/libmuparser.a
 
 # Optional include file to override default variables
@@ -40,7 +38,6 @@ MUPARSER_LIB = ${LIBDIR}/libmuparser.a
 .PHONY: clean distclean
 
 # run_network executable
-#run_network: $(MATHUTILS_LIB) $(CVODE_LIB) $(GSL_LIB) $(MUPARSER_LIB)
 run_network: $(MATHUTILS_LIB) $(CVODE_LIB) $(MUPARSER_LIB)
 	mkdir -p $(NETWORK_BINDIR)
 	cd $(NETWORK_BINDIR); cmake $(CMAKELISTS_DIR); make;
@@ -48,32 +45,28 @@ run_network: $(MATHUTILS_LIB) $(CVODE_LIB) $(MUPARSER_LIB)
 	cp -f $(NETWORK_BINDIR)/run_network $(BNG_BINDIR)
 
 # libraries
-$(CVODE_LIB):  $(LIBSOURCE)/$(CVODE).tar.gz
+$(CVODE_LIB):  
+#$(LIBSOURCE)/$(CVODE).tar.gz
 	mkdir -p $(LIBDIR) $(INCDIR)
-	rm -rf $(CVODE)
-	tar -xzf $(LIBSOURCE)/$(CVODE).tar.gz
+#	rm -rf $(CVODE)
+#	tar -xzf $(LIBSOURCE)/$(CVODE).tar.gz
 	cd $(CVODE);  ./configure --prefix=$(CURDIR) --disable-shared;  make;  make install
 
-#$(GSL_LIB):  $(LIBSOURCE)/$(GSL).tar.gz
-#	mkdir -p $(LIBDIR) $(INCDIR)
-#	rm -rf $(GSL)
-#	tar -xzf $(LIBSOURCE)/$(GSL).tar.gz
-#	cd $(GSL);  ./configure --prefix=$(CURDIR) --disable-shared;  make;  make install
-
-$(MUPARSER_LIB):  $(LIBSOURCE)/$(MUPARSER).zip
+$(MUPARSER_LIB):  
+#$(LIBSOURCE)/$(MUPARSER).zip
 	mkdir -p $(LIBDIR) $(INCDIR)
-	rm -rf $(MUPARSER)
-	unzip $(LIBSOURCE)/$(MUPARSER).zip
+#	rm -rf $(MUPARSER)
+#	unzip $(LIBSOURCE)/$(MUPARSER).zip
 	cd $(MUPARSER); ./configure --prefix=$(CURDIR) --disable-shared;  make;  make install	
-### Mac users (OS X 10.9+) comment out line above and uncomment line below
-### Also, uncomment lines 6 and 66 in CMakelists.txt (those defining "-stdlib=libstdc++" flag)
-#	cd $(MUPARSER); ./configure CXX=g++-4.2 --prefix=$(CURDIR) --disable-shared;  make;  make install
 
-$(MATHUTILS_LIB):  $(LIBSOURCE)/$(MATHUTILS).tar.gz
+$(MATHUTILS_LIB):  
+#$(LIBSOURCE)/$(MATHUTILS).tar.gz
 	mkdir -p $(LIBDIR) $(INCDIR)
-	rm -rf $(MATHUTILS)
-	tar -xzf $(LIBSOURCE)/$(MATHUTILS).tar.gz
-	cd $(MATHUTILS); make
+#	rm -rf $(MATHUTILS)
+#	tar -xzf $(LIBSOURCE)/$(MATHUTILS).tar.gz
+	cd $(MATHUTILS); make; \
+	mv libmathutils.a $(CMAKELISTS_DIR)/$(LIBDIR); \
+	cp mathutils.h $(CMAKELISTS_DIR)/$(INCDIR)
 
 # clean scripts
 clean:
@@ -84,10 +77,6 @@ clean:
 	    cd ${CVODE} ;          \
 	    ${MAKE} clean ;        \
 	fi;
-#	if test -d ${GSL} ; then \
-#	    cd ${GSL} ;          \
-#	    ${MAKE} clean ;      \
-#	fi;
 	if test -d ${MUPARSER} ; then \
 	    cd ${MUPARSER} ;          \
 	    ${MAKE} clean ;           \
@@ -102,28 +91,9 @@ distclean:
 	    cd ${CVODE} ;          \
 	    ${MAKE} distclean ;    \
 	fi;
-	if test -d ${GSL} ; then \
-	    cd ${GSL} ;          \
-	    ${MAKE} distclean ;  \
-	fi;
 	if test -d ${MUPARSER} ; then \
 	    cd ${MUPARSER} ;          \
 	    ${MAKE} distclean ;       \
 	fi;
 
 
-#----!!!! For making distributions of source code
-
-#FILES=LICENSE INSTALL Makefile  network.h network.cpp PLA.h PLA.cpp Mathutils/ run_network.cpp HOW_TO_INSTALL README cvode_ser.tar.gz
-#SOURCES=LICENSE INSTALL Makefile network.h network.cpp PLA.h PLA.cpp run_network.cpp HOW_TO_INSTALL README
-
-#dist: $(FILES)
-#	tar czf dist.tgz $(FILES)
-
-#ci: $(FILES)
-#	ci -zLT -s- -u $(SOURCES)
-#	rcs -U $(SOURCES)
-#	chmod u+w $(SOURCES)
-
-#co: $(FILES)
-#	co -zLT -s- -u $(SOURCES)
