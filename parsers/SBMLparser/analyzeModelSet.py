b9bb15a1cbbb4c52f9a39e618d7d3bf51e1fe8e8
Jose-Juan Tapia <jjtapia@gmail.com>
2015-02-26 14:31:13 -0500

    adding tracking information to batch script
    Former-commit-id: bbbd69ac5f01c7a422b7c33f83ee332a8dfcb048

--- parsers/SBMLparser/analyzeModelSet.py
+++ parsers/SBMLparser/analyzeModelSet.py
@@ -4,7 +4,7 @@ import fnmatch
 import os
 from subprocess import call        
 import multiprocessing as mp
-
+import progressbar
 def callSBML(filename):
     pass
 
@@ -16,7 +16,13 @@ def getFiles(directory,extension):
     matches = []
     for root, dirnames, filenames in os.walk(directory):
         for filename in fnmatch.filter(filenames, '*.{0}'.format(extension)):
-            matches.append(os.path.join(root, filename))
+            matches.append([os.path.join(root, filename),os.path.getsize(os.path.join(root, filename))])
+
+    #sort by size
+    matches.sort(key=lambda filename: filename[1], reverse=False)
+    
+    matches = [x[0] for x in matches]
+
     return matches
 
 def callSBMLTranslator(fileName,outputdirectory):
@@ -36,17 +42,21 @@ if __name__ == "__main__":
     outputdirectory = 'new_non_curated'
     restart = False
     filenameset = getFiles(directory,'xml')
-
+    
     if not restart:
         existingset = getFiles(outputdirectory,'bngl')
         existingset = ['.'.join(x.split('.')[:-1]).split('/')[-1] for x in existingset]
         filenameset = [x for x in filenameset if x.split('/')[-1] not in existingset]
 
-    print len(filenameset)
+    print(len(filenameset))
     futures = []
     workers = mp.cpu_count()*2
+    progress = progressbar.ProgressBar(maxval= len(filenameset)).start()
+    i = 0
     with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
         for filename in filenameset:
           futures.append(executor.submit(callSBMLTranslator, filename,outputdirectory))
         for future in concurrent.futures.as_completed(futures,timeout=3000):
-            pass
+            i+=1
+            progress.update(i)
+    progress.finish()
