1af0bb34c0beb2feba609ccd7634615eded5095e
Jose-Juan Tapia <jjtapia@gmail.com>
2015-04-26 12:31:04 -0400

    adding collapsed contact map processing. minor refactoring

--- stats/collapsedContactMap.py
+++ stats/collapsedContactMap.py
@@ -1,7 +1,12 @@
 from readBNGXML import parseXML
 import networkx as nx
 import argparse
-
+import pygraphviz as pgv
+import sys
+import os
+sys.path.insert(0,'./SBMLparser')
+from rulifier import componentGroups
+import difflib
 def extractMolecules(action,site1,site2,chemicalArray):
     '''
     this method goes through the chemicals in a given array 'chemicalArray'
@@ -93,7 +98,60 @@ def extractSingleTransformation(rule):
     return atomicArray, transformationCenter, transformationContext, productElements,actionName,label
 
 
-def createCollapsedContact(rules,species,transformations,fileName):
+def testForIntersection(mutualExclusion,mutualExclusionList,mutualExclusionDictionary):
+    for exclusion in mutualExclusionDictionary:
+        pivot = None
+        if mutualExclusion[0] in exclusion:
+            pivot = mutualExclusion[1]
+        elif mutualExclusion[1] in exclusion:
+            pivot = mutualExclusion[0]
+        if pivot:
+            exclusionCopy = exclusion[:]
+            otherExclusionList = []
+            for exclusionCandidate in mutualExclusionList:
+                if pivot in exclusionCandidate:
+                    otherChemical = [x for x in exclusionCandidate if x!= pivot][0]
+                    if otherChemical in exclusionCopy:
+                        exclusionCopy.remove(otherChemical)
+                        otherExclusionList.append(exclusionCandidate)
+            if len(exclusionCopy) == 0:
+                exclusion.append(pivot)
+                return otherExclusionList
+                
+    return False
+
+def processExtendedInformation(extendedInformation):
+
+    mutualExclusionDictionary = {}
+    for molecule in extendedInformation:
+        tmpmutualExclusion = []
+        idx =0 
+        exclusionList = list(extendedInformation[molecule]['mutualExclusion'])
+        while idx<len(exclusionList):
+            intersection = testForIntersection(list(exclusionList[idx]),exclusionList, tmpmutualExclusion)
+            if not intersection:
+                tmpmutualExclusion.append(list(exclusionList[idx]))
+                exclusionList.remove(exclusionList[idx])
+                idx = 0
+            else:
+                for individualIntersection in intersection:
+                    exclusionList.remove(individualIntersection)
+                    idx = 0
+        mutualExclusionDictionary[molecule] = tmpmutualExclusion
+    return mutualExclusionDictionary
+
+
+def getDummyNode(bondpartner1,bondpartner2,collapsedComponents):
+    if bondpartner1 in collapsedComponents:
+        for collapsedcomponent in collapsedComponents[bondpartner1]:
+            if bondpartner2.lower() in collapsedcomponent:
+                return '/'.join(collapsedcomponent),'{0}({1})'.format(bondpartner1,'/'.join(collapsedcomponent)),"#FFDDDD"
+
+    return '_'.join([bondpartner1,bondpartner2]),bondpartner2.lower(),"#FFFFFF"
+    
+        
+
+def createCollapsedContact(rules,species,transformations,fileName,extendedInformation):
     '''
     creates a collapsed bipartite graph given a list of rules
     collapsed bipartite means that molecules are represented as structureless nodes and 
@@ -106,7 +164,6 @@ def createCollapsedContact(rules,species,transformations,fileName):
     or product in our ouput file
     '''
 
-
     graph = nx.DiGraph()
     speciesNodes =[]
     processNodes = []
@@ -114,7 +171,8 @@ def createCollapsedContact(rules,species,transformations,fileName):
         graph.add_node(speciesUnit.name,graphics={'type':'roundrectangle','fill':'#FFCC00'})
         speciesNodes.append(speciesUnit.name)
     mainidx = 0
-
+    speciesName = [x.name for x in species]
+    collapsedComponents = processExtendedInformation(extendedInformation)
     for rule,_,_,_ in rules:
         mainidx += 1
         nonatomicset = False
@@ -123,16 +181,31 @@ def createCollapsedContact(rules,species,transformations,fileName):
 
         atomicArray, transformationCenter, transformationContext, productElements,actionName,label =  extractSingleTransformation(rule)
 
+
         for idx in xrange(len(rule.actions)):
             if rule.actions[idx].action in ['Add','Delete']:
                 nonatomicset = True
             elif rule.actions[idx].action in ['AddBond']:
 
                 bondpartners = [x.split('(')[0] for x in transformationCenter[idx]]
+
                 if len(bondpartners) == 2:
-                    graph.add_edge(bondpartners[0],bondpartners[1],graphics={'fill':"#000000"})
+                    dummyNode,label1,fill1 =   getDummyNode(bondpartners[0],bondpartners[1],collapsedComponents)
+                    dummyNode2,label2,fill2 = getDummyNode(bondpartners[1],bondpartners[0],collapsedComponents)
+                    graph.add_node(dummyNode,
+                        graphics={'type': "circle",'fill':fill1},LabelGraphics={'text':label1})
+                    graph.add_node(dummyNode2,
+                        graphics={'type': "circle",'fill':fill2},LabelGraphics={'text':label2})
+
+                    graph.add_edge(bondpartners[0],dummyNode,graphics={'fill':"#000000"},weight=1)
+                    graph.add_edge(dummyNode,dummyNode2,graphics={'fill':"#000000"},weight=1)
+                    graph.add_edge(bondpartners[1],dummyNode2,graphics={'fill':"#000000"},weight=1)
                 elif len(bondpartners) == 1:
-                    graph.add_edge(bondpartners[0],bondpartners[0],graphics={'fill':"#000000"})
+                    dummyNode = '{0}_{0}'.format(bondpartners[0])
+                    graph.add_node(dummyNode,
+                        graphics={'type': "circle",'fill':"#FFFFFF"},LabelGraphics={'text':bondpartners[0]})
+                    graph.add_edge(bondpartners[0],dummyNode,graphics={'fill':"#000000"},weight=1)
+                    graph.add_edge(dummyNode,bondpartners[0],graphics={'fill':"#000000"},weight=1)
                 else:
                     nonatomicset = True
                 for x in bondpartners:
@@ -145,7 +218,7 @@ def createCollapsedContact(rules,species,transformations,fileName):
                 state = [x.split('(')[1].split('~')[0] for x in transformationCenter[idx]]
                 graph.add_node(molecule[0] +'_'+state[0],graphics={'type': "circle",'fill':"#CCFFCC"},LabelGraphics={'text':state[0]})
                 processNodes.append(state[0])
-                graph.add_edge(molecule[0]+'_'+state[0],molecule[0],graphics={'fill':"#000000"})
+                graph.add_edge(molecule[0]+'_'+state[0],molecule[0],graphics={'fill':"#000000"},weight=0.7)
                 if molecule[0] in activeReactants:
                     activeReactants.remove(molecule[0])
                 if molecule[0] in activeProducts:
@@ -159,6 +232,42 @@ def createCollapsedContact(rules,species,transformations,fileName):
                 graph.add_edge(element,mainidx,graphics={'targetArrow': "standard"})
             for element in activeProducts:
                 graph.add_edge(mainidx,element)
+
+
+    color = {'requirement':'#0000FF','exclusion':'#FF0000','mutualExclusion':'#FF0000'}
+
+    #deal with information in extendedInformation
+    for molecule in extendedInformation:
+        for relationship in extendedInformation[molecule]:
+            for requirement in extendedInformation[molecule][relationship]:
+                if relationship == 'mutualExclusion':
+                    requirement1 = list(requirement)[0]
+                    requirement2 = list(requirement)[1]
+                elif relationship == 'nullexclusion':
+                    continue
+                else:
+                    requirement1 = requirement[0][0]
+                    requirement2 = requirement[1][0]
+                index1 = [i for i,x in enumerate(speciesName) if x.lower() == requirement1]
+                index2 = [i for i,x in enumerate(speciesName) if x.lower() == requirement2]
+                node1 = 1 if len(index1) > 0 else '{0}_{1}'.format(molecule,requirement1)
+                node2 = 1 if len(index2) > 0 else '{0}_{1}'.format(molecule,requirement2)
+                if node1 == 1:
+                    node1,_,_ = getDummyNode(molecule,speciesName[index1[0]],collapsedComponents)
+                if node2 == 1:
+                    node2,_,_ = getDummyNode(molecule,speciesName[index2[0]],collapsedComponents)
+                if relationship != 'mutualExclusion':  
+                    #pass
+                    graph.add_edge(node1,node2,graphics={'fill':color[relationship],'style':"dashed",'targetArrow':"standard"},weight=0.1)
+                else:
+                    #if relationship == 'exclusion':
+                    pass
+                    #graph.add_edge(node1,node2,graphics={'fill':color[relationship],'style':"dashed"},weight=0.1)
+            
+    
+    #layout nx.spring_layout(graph)
+    #for element in graph.nodes():
+
     nx.write_gml(graph,fileName)
 
 
@@ -166,12 +275,15 @@ def defineConsole():
     parser = argparse.ArgumentParser(description='SBML to BNGL translator')
     parser.add_argument('-i','--input',type=str,help='settings file',required=True)
     parser.add_argument('-o','--output',type=str,help='output directory')
+
+    parser.add_argument('-r','--rulify',action='store_true')
     return parser    
 
 
-def main(fileName,outputfilename):
+def main(fileName,outputfilename,extendedInformation):
     molecules,rules,_ = parseXML(fileName)
-    createCollapsedContact(rules,molecules,[1],outputfilename)         
+    createCollapsedContact(rules,molecules,[1],outputfilename,extendedInformation)         
+
 
 if __name__ == "__main__":
     parser = defineConsole()
@@ -181,5 +293,11 @@ if __name__ == "__main__":
         outputFile = namespace.output
     else:
         outputFile = inputFile + '.gml'
-    main(inputFile,outputFile)
+    if namespace.rulify:
+        extendedInformation = componentGroups.getContextRequirements(inputFile)   
+    else:
+        extendedInformation = {}
+    #print extendedInformation['EGFR']
+    main(inputFile,outputFile,extendedInformation)
+    
     #addAnnotations('fceri_ji')
