4c36ca9ba2fb0721083c897db2575d966036e8da
Leonard A. Harris <lh64@cornell.edu>
2017-05-14 12:19:26 -0500

    Updated Expression::toCVodeString() to support all built-in functions. This required replacing 'ln' with 'log', 'min' with 'fmin' and 'max' with 'fmax'. Special handling was also required for _pi(), _e(), if(), sum(), and avg().

--- bng2/Perl2/Expression.pm
+++ bng2/Perl2/Expression.pm
@@ -1423,28 +1423,54 @@ sub toCVodeString
             unless (defined $local_fcn) { die "Error in Expression->toMatlabString(): some problem evaluating anonymous function"; }
             $string = $local_fcn->Expr->toCVodeString($plist, $level+1, $expand);
         }
-        elsif ( exists $functions{ $expr->Arglist->[0] } )
-        {   # this is a built-in function
-            # handle built-ins with 1 argument that have the same name in the C library
-            if ( $fcn_name =~ /^(sin|cos|exp|log|abs|sqrt|floor|ceil)$/ )
-            {
-                my @sarr = ( map {$_->toCVodeString($plist, $level+1, $expand)} @{$expr->Arglist}[1..$#{$expr->Arglist}] );
-                $string = $fcn_name .'('. join( ',', @sarr ) .')';
-            }
-            # handle the 'if' built-in with 3 arguments
+        elsif ( isBuiltIn($fcn_name) )
+        {
+        		# convert '_pi()' to 'pi'
+            if ($fcn_name eq '_pi'){ $string = 'M_PI'; }
+            
+        		# convert '_e()' to 'exp(1)'
+            elsif ( $fcn_name eq '_e' ){ $string = "M_E"; }
+            
+        		# handle the 'if' built-in with 3 arguments
             elsif ( $fcn_name eq 'if' )
             {   
                 # substitute the "?" operator for the if function
                 my @sarr = ( map {$_->toCVodeString($plist, $level+1)} @{$expr->Arglist}[1..$#{$expr->Arglist}] );
 
-                if ( @sarr == 3)
-                {   $string = '('. $sarr[0] .' ? '. $sarr[1] .' : '. $sarr[2] .')';   }
+                if ( @sarr == 3 )
+                {   $string = '('. $sarr[0] .' ? '. $sarr[1] .' : '. $sarr[2] .')'; }
                 else
                 {   die "Error in Expression->toCVodeString():  built-in function 'if' must have three arguments!";   }    
             }
-            # fatal error if the built-in is not handled above
+            
+            # handle the 'sum' and 'avg' built-ins with >=1 argument
+            elsif( $fcn_name eq 'sum' or $fcn_name eq 'avg')
+            {
+            		my @sarr = ( map {$_->toCVodeString($plist, $level+1)} @{$expr->Arglist}[1..$#{$expr->Arglist}] );
+            		if ( @sarr ge 1 )
+                {   
+                		$string = '('. $sarr[0];
+                		for(my $i=1; $i < scalar(@sarr); $i++){
+				    		$string .= '+'. $sarr[$i];
+				    }
+                		$string .= ')';
+                		# if 'avg', divide sum by number of elements
+                		if ( $fcn_name eq 'avg' )
+                		{ $string = '(' . $string . '/' . scalar(@sarr) . ')'; }
+                }
+                else
+                {   die "Error in Expression->toCVodeString():  built-in function $fcn_name must have at least 1 argument!";   }  
+            }
+            
+            # all other built-ins
             else
-            {   die "Error in Expression->toCVodeString():  don't know how to handle built-in function $fcn_name!";   }
+            {
+				if ($fcn_name eq 'ln'){ $fcn_name = 'log'; } # change 'ln' to 'log'
+				if ($fcn_name eq 'min'){ $fcn_name = 'fmin'; } # change 'min' to 'fmin'
+				if ($fcn_name eq 'max'){ $fcn_name = 'fmax'; } # change 'max' to 'fmax'
+                my @sarr = ( map {$_->toCVodeString($plist, $level+1, $expand)} @{$expr->Arglist}[1..$#{$expr->Arglist}] );
+                $string = $fcn_name .'('. join( ',', @sarr ) .')';
+            }
         }
         else
         {   # user-defined function or observable
