ce704112f81877f2264130f8c5d8b3e7de799876
Robert Clark <rhclark.pitt@gmail.com>
2017-05-30 14:42:21 -0400

    change platform name
    change platform name

--- bng2/make_html.pl
+++ bng2/make_html.pl
@@ -160,10 +160,10 @@ open(FNEW,">$ofile_name");
   print FNEW "<tr>\n";
   print FNEW "<td><center>If you would like to download only the NFsim executable, click here:</center>\n";
   print FNEW "<br><center>\n";
-  print FNEW "<a href=\"NFsim-".$travis_os.".html\"><font size=\"+2\">NFsim-".$travis_os."</font></a></center></td>\n";
+  print FNEW "<a href=\"NFsim-".$platform.".html\"><font size=\"+2\">NFsim-".$platform."</font></a></center></td>\n";
   print FNEW "<td><center>If you would like to download only the Atomizer executable, click here:</center>\n";
   print FNEW "<br><center>\n";
-  print FNEW "<a href=\"sbmlTranslator-".$travis_os.".html\"><font size=\"+2\">sbmlTranslator-".$travis_os."</font></a></center></td>\n";
+  print FNEW "<a href=\"sbmlTranslator-".$platform.".html\"><font size=\"+2\">sbmlTranslator-".$platform."</font></a></center></td>\n";
   print FNEW "</tr>\n";
   print FNEW "</table>\n";
 
