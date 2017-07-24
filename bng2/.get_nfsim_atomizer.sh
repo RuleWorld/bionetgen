b8f10956dda005e216061df8d37a2f9b491c192c
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 15:14:56 -0400

    Fix executable names.
    Fix executable names.

--- bng2/.get_nfsim_atomizer.sh
+++ bng2/.get_nfsim_atomizer.sh
@@ -4,9 +4,9 @@
 
   if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
-    curl -O  -v -u $FTP_USER_0:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
+    curl -O  -v -u $FTP_USER_0:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
   else
-    curl -O  -v -u $FTP_USER_1:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
+    curl -O  -v -u $FTP_USER_1:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
   fi
   mv       NFsim-$platform_travis  $1/NFsim
   chmod +x $1/NFsim
