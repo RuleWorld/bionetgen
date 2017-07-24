eb1e50019414ad9f9b05ead14344cbc3f1901d16
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 15:03:02 -0400

    New passwords.
    New passwords.

--- bng2/.get_nfsim_atomizer.sh
+++ bng2/.get_nfsim_atomizer.sh
@@ -4,9 +4,9 @@
 
   if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
-    curl -O  ftp://15260@www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
+    curl -O  -v -u $FTP_USER_0:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
   else
-    curl -O  ftp://15261@www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
+    curl -O  -v -u $FTP_USER_1:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
   fi
   mv       NFsim-$platform_travis  $1/NFsim
   chmod +x $1/NFsim
