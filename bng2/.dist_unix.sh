0f2b1712d683967a930a3b2211031181050cc402
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 16:59:29 -0400

    Put in Atomizer changes
    Put in Atomizer changes

--- bng2/.dist_unix.sh
+++ bng2/.dist_unix.sh
@@ -27,7 +27,7 @@ else
 fi
 
 
-#  Get the NFsim and Atomizer files that are needed
+#  Get the NFsim files that are needed
 git clone https://github.com/ruleworld/nfsim.git
 #  curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-source-$platform_travis.tar.gz
 cd  $vbase
@@ -39,6 +39,16 @@ cd ../$vbase/source_NFsim
   cd ..
 cd ..
 
+
+#  Get the Atomizer files that are needed
+git clone https://github.com/ruleworld/atomizer.git
+#  curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-source-$platform_travis.tar.gz
+cd  $vbase
+mkdir source_Atomizer
+cd ../atomizer
+cp -r appveyor  Makefile requirements.txt  twistedServer.py config  SBMLparser  utils ../$vbase/source_Atomizer
+cp -r dist reactionDefinitions  stats  gml2sbgn  README.md   test  XMLExamples        ../$vbase/source_Atomizer
+cd ..
 
 
 tar  cvf $vbase.tar $vbase
