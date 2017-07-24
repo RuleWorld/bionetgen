ccee317d2efc06218123bc6b87d1c378b2cbbb8b
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 15:26:37 -0400

    Fix curl statement
    Fix curl statement

--- bng2/.get_nfsim_atomizer.sh
+++ bng2/.get_nfsim_atomizer.sh
@@ -4,11 +4,11 @@
 
   if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
-    curl -O  -v -u $FTP_USER_0:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
+    curl -v -u $FTP_USER_0:$FTP_PASSWORD  -O ftp://www.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
   else
-    curl -O  -v -u $FTP_USER_1:$FTP_PASSWORD  ftp://www.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
+    curl -v -u $FTP_USER_1:$FTP_PASSWORD  -O ftp://www.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
   fi
-  mv       NFsim-$platform_travis  $1/NFsim
+  mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
   chmod +x $1/NFsim
 
 
