699febbbadd28ab08c1dbcad77eb57d4927a5eec
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 14:30:47 -0400

    Small change.
    Small change.

--- bng2/.dist_unix.sh
+++ bng2/.dist_unix.sh
@@ -37,14 +37,6 @@ cp -r bin doc models src test tools validate CMakeLists.txt LICENSE.txt README.t
 cd ../$vbase/source_NFsim
   rm  -f    ./validate/*.tar.bz2
   cd ..
-  if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
-    curl -O  ftp://15260@www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
-  else
-    curl -O  ftp://15261@www.drivehq.com/d_data/d_travis/NFsim-$platform_travis
-  fi
-  mv       NFsim-$platform_travis  ./bin/NFsim
-  chmod    +x                      ./bin/NFsim
 cd ..
 
 
