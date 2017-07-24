84d4a8789c7dedd7bd03e2e9b445274a2fbc0fe4
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 16:28:17 -0400

    Get Atomizer
    Get Atomizer

--- bng2/.get_nfsim_atomizer.sh
+++ bng2/.get_nfsim_atomizer.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# First get NFsim
 
   if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
     # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
     curl -v -u $FTP_USER_0:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
+    curl -v -u $FTP_USER_0:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/sbmlTranslator-${TRAVIS_OS_NAME}
   else
     curl -v -u $FTP_USER_1:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
+    curl -v -u $FTP_USER_1:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/sbmlTranslator-${TRAVIS_OS_NAME}
   fi
+  
   mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
   chmod +x $1/NFsim
 
+  
+  mv       sbmlTranslator-${TRAVIS_OS_NAME}  $1/sbmlTranslator
+  chmod +x $1/sbmlTranslator
 
-# Next get Atomizer
