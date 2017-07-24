297a214ded7abf784ee81487d8622daee9b2f1d9
Robert Clark <rhclark.pitt@gmail.com>
2017-07-24 19:52:28 -0400

    Add WIndows platforms
    Add WIndows platforms

--- bng2/.get_nfsim_atomizer.sh
+++ bng2/.get_nfsim_atomizer.sh
@@ -5,15 +5,32 @@
     # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
     curl -v -u $FTP_USER_0:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
     curl -v -u $FTP_USER_0:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/sbmlTranslator-${TRAVIS_OS_NAME}
-  else
+    mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
+    chmod +x $1/NFsim
+    mv       sbmlTranslator-${TRAVIS_OS_NAME}  $1/sbmlTranslator
+    chmod +x $1/sbmlTranslator
+  fi
+  if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
     curl -v -u $FTP_USER_1:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
     curl -v -u $FTP_USER_1:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/sbmlTranslator-${TRAVIS_OS_NAME}
+    mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
+    chmod +x $1/NFsim
+    mv       sbmlTranslator-${TRAVIS_OS_NAME}  $1/sbmlTranslator
+    chmod +x $1/sbmlTranslator
   fi
   
-  mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
-  chmod +x $1/NFsim
-
   
-  mv       sbmlTranslator-${TRAVIS_OS_NAME}  $1/sbmlTranslator
-  chmod +x $1/sbmlTranslator
+  if [ "$2" = "x86" ]; then
+    curl -v -u 15262:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-Win32.exe
+    curl -v -u 15262:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/sbmlTranslator-Win32.exe
+    mv       NFsim-Win32.exe           $1/NFsim.exe
+    mv       sbmlTranslator-Win32.exe  $1/sbmlTranslator.exe
+  fi
+
+  if [ "$2" = "x64" ]; then
+    curl -v -u 15263:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-Win64.exe
+    curl -v -u 15263:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/sbmlTranslator-Win64.exe
+    mv       NFsim-Win64.exe           $1/NFsim.exe
+    mv       sbmlTranslator-Win64.exe  $1/sbmlTranslator.exe
+  fi
 
