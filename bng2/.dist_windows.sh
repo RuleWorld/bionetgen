52bd3687db3bfe435f0939a01c54b9559b0bea15
Robert Clark <rhclark.pitt@gmail.com>
2017-07-25 18:19:40 -0400

    Copy source for NFsim and Atomizer

--- bng2/.dist_windows.sh
+++ bng2/.dist_windows.sh
@@ -56,6 +56,35 @@ cd ..
 
 
 
+
+#  Get the NFsim files that are needed
+#  git clone https://github.com/ruleworld/nfsim.git  This clone command had to be moved to an earlier spot in YAML.
+#  curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-source-$platform_travis.tar.gz
+cd  $vbase
+mkdir source_NFsim
+cd ../nfsim
+cp -r bin doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ../$vbase/source_NFsim
+cd ../$vbase/source_NFsim
+  rm  -f    ./validate/*.tar.bz2
+  cd ..
+cd ..
+
+
+#  Get the Atomizer files that are needed
+# git clone https://github.com/ruleworld/atomizer.git
+#  curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-source-$platform_travis.tar.gz
+cd  $vbase
+mkdir source_Atomizer
+cd ../atomizer
+cp -r appveyor  Makefile requirements.txt  twistedServer.py config  SBMLparser  utils ../$vbase/source_Atomizer
+cp -r dist reactionDefinitions  stats  gml2sbgn  README.md   test  XMLExamples        ../$vbase/source_Atomizer
+cd ..
+
+
+
+
+
+
 zip -r -q  $rall $vbase
 
 echo " Version base name is      " $vbase
