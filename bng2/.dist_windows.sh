1f0ec2547d1f4f5860d7bce6186a943c57eb89ce
Robert Clark <rhclark.pitt@gmail.com>
2017-07-26 11:12:44 -0400

    Correct cd statement

--- bng2/.dist_windows.sh
+++ bng2/.dist_windows.sh
@@ -65,7 +65,7 @@ cd ../../nfsim
 cp -r bin doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ../bng2/$vbase/source_NFsim
 cd ../bng2/$vbase/source_NFsim
   rm  -f    ./validate/*.tar.bz2
-cd ../../..
+cd ../..
 
 
 #  Get the Atomizer files that are needed
