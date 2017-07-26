4b772c6d7274859fcbc400fbf29dd7bf5f9e113a
Robert Clark <rhclark.pitt@gmail.com>
2017-07-25 20:48:16 -0400

    Find correct directories

--- bng2/.dist_windows.sh
+++ bng2/.dist_windows.sh
@@ -55,27 +55,17 @@ mv       sbmlTranslator-$platform.exe                 ./bin/sbmlTranslator.exe
 cd ..
 
 
-echo '  '
-echo '  '
-ls 
-echo '  '
-echo '  '
-ls ..
-echo '  '
-echo '  '
-
 
 #  Get the NFsim files that are needed
 #  git clone https://github.com/ruleworld/nfsim.git  This clone command had to be moved to an earlier spot in YAML.
 #  curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-source-$platform_travis.tar.gz
 cd  $vbase
 mkdir source_NFsim
-cd ../nfsim
-cp -r bin doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ../$vbase/source_NFsim
-cd ../$vbase/source_NFsim
+cd ../../nfsim
+cp -r bin doc models src test tools validate CMakeLists.txt LICENSE.txt README.txt makefile NFsim_manual_v1.12.pdf ../bng2/$vbase/source_NFsim
+cd ../bng2/$vbase/source_NFsim
   rm  -f    ./validate/*.tar.bz2
-  cd ..
-cd ..
+cd ../../..
 
 
 #  Get the Atomizer files that are needed
@@ -83,13 +73,10 @@ cd ..
 #  curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-source-$platform_travis.tar.gz
 cd  $vbase
 mkdir source_Atomizer
-cd ../atomizer
-cp -r appveyor  Makefile requirements.txt  twistedServer.py config  SBMLparser  utils ../$vbase/source_Atomizer
-cp -r dist reactionDefinitions  stats  gml2sbgn  README.md   test  XMLExamples        ../$vbase/source_Atomizer
-cd ..
-
-
-
+cd ../../atomizer
+cp -r appveyor  Makefile requirements.txt  twistedServer.py config  SBMLparser  utils ../bng2/$vbase/source_Atomizer
+cp -r dist reactionDefinitions  stats  gml2sbgn  README.md   test  XMLExamples        ../bng2/$vbase/source_Atomizer
+cd ../..
 
 
 
@@ -103,7 +90,3 @@ ls -l
 # native Appveyor deployment system, which permits you to use ftp.
 #curl -T $rall  -u roberthclark:xxxxxxxxxx ftp://ftp.midcapsignals.com/midcap/junk/$rall
 
-# Move a simple HTML page over to the server, to provide a pointer to the distribution package
-perl .make_html.pl  --version $vname  --platform $platform
-html_name="BioNetGen-"$platform".html"
-#curl -T $html_name -u roberthclark:xxxxxxxxxx ftp://ftp.midcapsignals.com/midcap/junk/
