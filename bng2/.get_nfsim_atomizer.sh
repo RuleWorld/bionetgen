edaf00d41f25bdd7da503f7e27d8bf5305f1a227
Robert Clark <rhclark.pitt@gmail.com>
2017-08-01 20:36:47 -0400

    Try Box

--- bng2/.get_nfsim_atomizer.sh
+++ bng2/.get_nfsim_atomizer.sh
@@ -9,8 +9,7 @@
 
 
   if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
-    # curl -O  http://www.midcapsignals.com/midcap/junk/NFsim-$platform_travis
-    curl -v -u $FTP_USER_4:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD  -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
     mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
     chmod +x $1/NFsim
 
@@ -18,12 +17,14 @@
 #    mv       sbmlTranslator-${TRAVIS_OS_NAME}  $1/sbmlTranslator
 #    chmod +x $1/sbmlTranslator
 
-    curl -v -u $FTP_USER_4:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/Linux/sbmlTranslator
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD  -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_bin-2.3.0/Linux/sbmlTranslator
     mv       sbmlTranslator               $1/sbmlTranslator
     chmod +x $1/sbmlTranslator
   fi
+
+
   if [ "${TRAVIS_OS_NAME}" = "osx" ]; then
-    curl -v -u $FTP_USER_5:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD  -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_travis/NFsim-${TRAVIS_OS_NAME}
     mv       NFsim-${TRAVIS_OS_NAME}  $1/NFsim
     chmod +x $1/NFsim
     
@@ -31,31 +32,31 @@
 #    mv       sbmlTranslator-${TRAVIS_OS_NAME}  $1/sbmlTranslator
 #    chmod +x $1/sbmlTranslator
 
-    curl -v -u $FTP_USER_5:$FTP_PASSWORD  -O ftp://ftp.drivehq.com/MacOSX/sbmlTranslator
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD  -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_bin-2.3.0/MacOSX/sbmlTranslator
     mv       sbmlTranslator               $1/sbmlTranslator
     chmod +x $1/sbmlTranslator
   fi
   
   
   if [ "$2" = "x86" ]; then
-    curl -v -u 15262:$3  -O ftp://ftp.drivehq.com/d_data/d_appveyor/build/NFsim-Win32.exe
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD  -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_appveyor/build/NFsim-Win32.exe
     mv       NFsim-Win32.exe           $1/NFsim.exe
 
 #    curl -v -u 15262:$3  -O ftp://ftp.drivehq.com/d_data/d_appveyor/dist/sbmlTranslator-Win32.exe
 #    mv       sbmlTranslator-Win32.exe  $1/sbmlTranslator.exe
 
-     curl -v -u 15262:$3  -O ftp://ftp.drivehq.com/Win32/sbmlTranslator_64.exe
-     mv ./sbmlTranslator_64.exe  $1/sbmlTranslator_64.exe
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_bin-2.3.0/Win32/sbmlTranslator.exe
+    mv ./sbmlTranslator.exe  $1/sbmlTranslator_64.exe
   fi
 
   if [ "$2" = "x64" ]; then
-    curl -v -u 15263:$3  -O ftp://ftp.drivehq.com/d_data/d_appveyor/build/NFsim-Win64.exe
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_appveyor/build/NFsim-Win64.exe
     mv       NFsim-Win64.exe           $1/NFsim.exe
 
 #    curl -v -u 15263:$3  -O ftp://ftp.drivehq.com/d_data/d_appveyor/dist/sbmlTranslator-Win64.exe
 #    mv       sbmlTranslator-Win64.exe  $1/sbmlTranslator.exe
 
-    curl -v -u 15263:$3  -O ftp://ftp.drivehq.com/Win64/sbmlTranslator.exe
+    curl -v --ftp-ssl -u $FTP_USER:$FTP_PASSWORD -O ftp://ftp.box.com/BioNetGen_Beta/d_data/d_bin-2.3.0/Win64/sbmlTranslator.exe
     mv ./sbmlTranslator.exe  $1/sbmlTranslator.exe
   fi
 
