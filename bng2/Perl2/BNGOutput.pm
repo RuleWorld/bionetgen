a5d87d00a37f0e2a7742932c1d5d04b33af4e6f0
Sanjana Gupta <sag134@pitt.edu>
2018-03-02 14:52:41 -0500

    restructuring the c file from writeCfile. Integration happens in 'integrate' function called by main. Adding code to read default values for parameters, initial species, time points from the BNG file

--- bng2/Perl2/BNGOutput.pm
+++ bng2/Perl2/BNGOutput.pm
@@ -3548,7 +3548,6 @@ sub writeCfile
                               ."        CVodeFree(&cvode_mem);         \n"
                               ."        return 1;                      \n"
                               ."    }                                  ";
-    print "1\n";
     # Index parameters associated with Constants, ConstantExpressions and Observables
     ($err) = $plist->indexParams();
     if ($err) { return $err };
@@ -3586,7 +3585,6 @@ sub writeCfile
 		    $user_fcn_definitions .= $fcn_defn . "\n";
         }
 	}	
-	print "2\n";
     # index reactions
     ($err) = $model->RxnList->updateIndex( $plist );
     if ($err) { return $err };
@@ -3601,8 +3599,28 @@ sub writeCfile
 	# retrieve a string of species deriv definitions
     my $calc_derivs_string;
     ($calc_derivs_string, $err) = $model->SpeciesList->toCVodeString( $model->RxnList, $stoich_hash, $plist );
-    if ($err) { return $err };   	
-    # open Mexfile and begin printing...
+    if ($err) { return $err };   
+
+	my $c_param_names;
+	my $c_param_values;
+	($c_param_names, $c_param_values, $err) = $plist->getMatlabConstantNames();
+    if ($err) { return $err };
+    # get number of species
+    my $n_species = scalar @{$model->SpeciesList->Array};
+    # time options
+    my $t_start = 0;
+    if ( exists $params->{'t_start'} )
+    {   $t_start = $params->{'t_start'};  }  
+
+    my $t_end = 10;
+    if ( exists $params->{'t_end'} )
+    {   $t_end = $params->{'t_end'};  } 
+
+    my $n_steps = 20;
+    if ( exists $params->{'n_steps'} )
+    {   $n_steps = $params->{'n_steps'};  } 
+
+    # open Cfile and begin printing...
 	open( c_file, ">$c_path" ) or die "Couldn't open $c_path: $!\n";
     print c_file <<"EOF";
 /* Library headers */
@@ -3626,7 +3644,8 @@ void  calc_expressions ( N_Vector expressions, double * parameters );
 void  calc_observables ( N_Vector observables, N_Vector species, N_Vector expressions );
 void  calc_ratelaws    ( N_Vector ratelaws,  N_Vector species, N_Vector expressions, N_Vector observables );
 int   calc_species_deriv ( realtype time, N_Vector species, N_Vector Dspecies, void * f_data );
-
+double* initialize_species(double* params,int nspecies);
+double * integrate(double* timepoints, double* species_init, double* params, int num_eq, int n_timepoints);
 /* user-defined function declarations */
 $user_fcn_declarations
 
@@ -3683,12 +3702,236 @@ $calc_derivs_string
     return(0);
 }
 
+//function to return pointer to integrated species output
+double * integrate(double* timepoints, double* species_init, double* params, int num_eq, int n_timepoints)
+{
+    size_t i;
+    size_t j;
+    double *species_out = (double *)malloc((n_timepoints+1)*(num_eq+1)* sizeof(double));
+    int counter = 0;
+    //[n_timepoints][num_eq+1];
+    /* intermediate data vectors */
+    N_Vector  expressions;
+    N_Vector  observables;
+    N_Vector  ratelaws;
+    /* array to hold pointers to data vectors */
+    N_Vector  temp_data[3];
+    /* CVODE specific variables */
+    realtype  reltol;
+    realtype  abstol;
+    realtype  time;
+    N_Vector  species;
+    void *    cvode_mem;
+    int       flag; 
+    /* initialize intermediate data vectors */
+    expressions  = NULL;
+    expressions = N_VNew_Serial(__N_EXPRESSIONS__);
+    /*if (check_flag((void *)expressions, "N_VNew_Serial", 0))
+    {
+        return 1;
+    }*/
+    observables = NULL;
+    observables = N_VNew_Serial(__N_OBSERVABLES__);
+    
+    /*if (check_flag((void *)observables, "N_VNew_Serial", 0))
+    {
+        N_VDestroy_Serial(expressions);
+        return 1;
+    }*/
+    ratelaws    = NULL; 
+    ratelaws = N_VNew_Serial(__N_RATELAWS__);
+    /*
+    if (check_flag((void *)ratelaws, "N_VNew_Serial", 0))
+    {   
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);        
+        return 1;
+    } */ 
+    /* set up pointers to intermediate data vectors */
+    temp_data[0] = expressions;
+    temp_data[1] = observables;
+    temp_data[2] = ratelaws;
+    /* calculate expressions (expressions are constant, so only do this once!) */
+    calc_expressions( expressions, params );       
+    /* SOLVE model equations! */
+    species   = NULL;
+    cvode_mem = NULL;
+    /* Set the scalar relative tolerance */
+    reltol = 1e-08;
+    abstol = 1e-06;
+    /* Create serial vector for Species */
+    species = N_VNew_Serial(__N_SPECIES__);
+    if (check_flag((void *)species, "N_VNew_Serial", 0))
+    {  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);
+        //return 1;
+    }
+    for ( i = 0; i < __N_SPECIES__; i++ )
+    {   NV_Ith_S(species,i) = species_init[i];   }
+
+    /*   Call CVodeCreate to create the solver memory:    
+     *   CV_ADAMS or CV_BDF is the linear multistep method
+     *   CV_FUNCTIONAL or CV_NEWTON is the nonlinear solver iteration
+     *   A pointer to the integrator problem memory is returned and stored in cvode_mem.
+     */
+    cvode_mem = CVodeCreate(CV_BDF, CV_NEWTON);
+    if (check_flag((void *)cvode_mem, "CVodeCreate", 0))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+        //return 1;                      
+    }                                  
+    /*   Call CVodeInit to initialize the integrator memory:     
+     *   cvode_mem is the pointer to the integrator memory returned by CVodeCreate
+     *   rhs_func  is the user's right hand side function in y'=f(t,y)
+     *   T0        is the initial time
+     *   y         is the initial dependent variable vector
+     */
+    flag = CVodeInit(cvode_mem, calc_species_deriv, timepoints[0], species);
+    if (check_flag(&flag, "CVodeInit", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+       // return 1;                      
+    }                                    
+    /* Set scalar relative and absolute tolerances */
+    flag = CVodeSStolerances(cvode_mem, reltol, abstol);
+    if (check_flag(&flag, "CVodeSStolerances", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+    //    return 1;                      
+    }                                        
+    /* pass params to rhs_func */
+    flag = CVodeSetUserData(cvode_mem, &temp_data);
+    if (check_flag(&flag, "CVodeSetFdata", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+        //return 1;                      
+    }                                     
+    /* select linear solver */
+    flag = CVDense(cvode_mem, __N_SPECIES__);
+    if (check_flag(&flag, "CVDense", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+        //return 1;                      
+    }                                     
+    flag = CVodeSetMaxNumSteps(cvode_mem, 2000);
+    if (check_flag(&flag, "CVodeSetMaxNumSteps", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+       // return 1;                      
+    }                                  
+    flag = CVodeSetMaxErrTestFails(cvode_mem, 7);
+    if (check_flag(&flag, "CVodeSetMaxErrTestFails", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+        //return 1;                      
+    }                                  
+    flag = CVodeSetMaxConvFails(cvode_mem, 10);
+    if (check_flag(&flag, "CVodeSetMaxConvFails", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+        //return 1;                      
+    }                                  
+    flag = CVodeSetMaxStep(cvode_mem, 0.0);
+    if (check_flag(&flag, "CVodeSetMaxStep", 1))
+    {                                  
+        N_VDestroy_Serial(expressions);
+        N_VDestroy_Serial(observables);
+        N_VDestroy_Serial(ratelaws);   
+        N_VDestroy_Serial(species);    
+        CVodeFree(&cvode_mem);         
+       // return 1;                      
+    }                   
+    //initializing output species array. First column will be time.               
+    species_out[counter] = timepoints[0]; 
+    //printf("%.16e\t",species_out[counter]);
+    counter++;
+    for(j=0;j<__N_SPECIES__;j++)
+    {
+        species_out[counter] = NV_Ith_S(species,j);
+        //printf("%.16e\t",species_out[counter]);
+        counter++;
+    }
+    //printf("\n");
+    /* integrate to each timepoint */
+    for ( i=1;  i <= n_timepoints;  i++ )
+    {
+        flag = CVode(cvode_mem, timepoints[i], species, &time, CV_NORMAL);
+        if (check_flag(&flag, "CVode", 1))
+        {
+            N_VDestroy_Serial(expressions);
+            N_VDestroy_Serial(observables);           
+            N_VDestroy_Serial(ratelaws);
+            N_VDestroy_Serial(species);
+            CVodeFree(&cvode_mem);
+            //return 1;
+        }  
+        species_out[counter] = timepoints[i];
+        //printf("%.16e\t",species_out[counter]);
+        counter++;
+        for(j=0;j<__N_SPECIES__;j++)
+        {
+            species_out[counter] = NV_Ith_S(species,j);
+            //printf("%.16e\t",species_out[counter]);
+            counter++;
+        }
+        //printf("\n");
+    }
+    /* Free vectors */
+    N_VDestroy_Serial(expressions);
+    N_VDestroy_Serial(observables);  
+    N_VDestroy_Serial(ratelaws);        
+    N_VDestroy_Serial(species);
+    /* Free integrator memory */
+    CVodeFree(&cvode_mem);
+    return species_out;
+}
+
+double* initialize_species(double* params,int nspecies)
+{
+    double* species_init = malloc(nspecies*sizeof(*species_init));
+    species_init[0] = 0;
+    return species_init;
+}
 int main(int argc,char** argv)
 {
     size_t    i;
-    size_t    j;
-    
+    //size_t    j;   
     /* user (command line input) variables */
+    /*
     double t0 = atof(argv[1]);
     double tend = atof(argv[2]);
     size_t n_timepoints = atoi(argv[3]);
@@ -3715,159 +3958,30 @@ int main(int argc,char** argv)
     	timepoints_array[i] = i*tend/(n_timepoints-1);
     }
     double *  timepoints = timepoints_array; 
-    /* intermediate data vectors */
-    N_Vector  expressions;
-    N_Vector  observables;
-    N_Vector  ratelaws;
-
-    /* array to hold pointers to data vectors */
-    N_Vector  temp_data[3];
-    
-    /* CVODE specific variables */
-    realtype  reltol;
-    realtype  abstol;
-    realtype  time;
-    N_Vector  species;
-    void *    cvode_mem;
-    int       flag;
-   
-    /* initialize intermediate data vectors */
-    expressions  = NULL;
-    expressions = N_VNew_Serial(__N_EXPRESSIONS__);
-    if (check_flag((void *)expressions, "N_VNew_Serial", 0))
+    */
+    double params[$n_parameters] = {$c_param_values};
+    double t0 = $t_start;
+    double tend = $t_end;
+    size_t n_timepoints = $n_steps;
+    double timepoints[n_timepoints];
+    int nspecies = $n_species;
+    for(i=1;i<=n_timepoints;i++)
     {
-        return 1;
+        timepoints[i] = t0+i*tend/(n_timepoints);
     }
-    observables = NULL;
-    observables = N_VNew_Serial(__N_OBSERVABLES__);
-    if (check_flag((void *)observables, "N_VNew_Serial", 0))
+    double *species_init = initialize_species(params,nspecies);
+    double *species_out = integrate(timepoints, species_init, params,nspecies,n_timepoints);
+    int j;
+    int counter=0;
+    for(i=0;i<n_timepoints+1;i++)
     {
-        N_VDestroy_Serial(expressions);
-        return 1;
-    }
-
-    ratelaws    = NULL; 
-    ratelaws = N_VNew_Serial(__N_RATELAWS__);
-    if (check_flag((void *)ratelaws, "N_VNew_Serial", 0))
-    {   
-        N_VDestroy_Serial(expressions);
-        N_VDestroy_Serial(observables);        
-        return 1;
-    }
-    
-    /* set up pointers to intermediate data vectors */
-    temp_data[0] = expressions;
-    temp_data[1] = observables;
-    temp_data[2] = ratelaws;
-
-    /* calculate expressions (expressions are constant, so only do this once!) */
-    calc_expressions( expressions, parameters );
-
-        
-    /* SOLVE model equations! */
-    species   = NULL;
-    cvode_mem = NULL;
-
-    /* Set the scalar relative tolerance */
-    reltol = $cvode_reltol;
-    abstol = $cvode_abstol;
-
-    /* Create serial vector for Species */
-    species = N_VNew_Serial(__N_SPECIES__);
-    if (check_flag((void *)species, "N_VNew_Serial", 0))
-    {  
-        N_VDestroy_Serial(expressions);
-        N_VDestroy_Serial(observables);
-        N_VDestroy_Serial(ratelaws);
-        return 1;
-    }
-    for ( i = 0; i < __N_SPECIES__; i++ )
-    {   NV_Ith_S(species,i) = species_init[i];   }
-
-    /*   Call CVodeCreate to create the solver memory:    
-     *   CV_ADAMS or CV_BDF is the linear multistep method
-     *   CV_FUNCTIONAL or CV_NEWTON is the nonlinear solver iteration
-     *   A pointer to the integrator problem memory is returned and stored in cvode_mem.
-     */
-    cvode_mem = CVodeCreate($cvode_linear_multistep, $cvode_nonlinear_solver);
-    if (check_flag((void *)cvode_mem, "CVodeCreate", 0))
-    $cvode_cleanup_memory
-    /*   Call CVodeInit to initialize the integrator memory:     
-     *   cvode_mem is the pointer to the integrator memory returned by CVodeCreate
-     *   rhs_func  is the user's right hand side function in y'=f(t,y)
-     *   T0        is the initial time
-     *   y         is the initial dependent variable vector
-     */
-    flag = CVodeInit(cvode_mem, calc_species_deriv, timepoints[0], species);
-    if (check_flag(&flag, "CVodeInit", 1))
-    $cvode_cleanup_memory
-   
-    /* Set scalar relative and absolute tolerances */
-    flag = CVodeSStolerances(cvode_mem, reltol, abstol);
-    if (check_flag(&flag, "CVodeSStolerances", 1))
-    $cvode_cleanup_memory   
-   
-    /* pass params to rhs_func */
-    flag = CVodeSetUserData(cvode_mem, &temp_data);
-    if (check_flag(&flag, "CVodeSetFdata", 1))
-    $cvode_cleanup_memory
-    
-    /* select linear solver */
-    $cvode_linear_solver
-    $cvode_cleanup_memory
-    
-    flag = CVodeSetMaxNumSteps(cvode_mem, $cvode_max_num_steps);
-    if (check_flag(&flag, "CVodeSetMaxNumSteps", 1))
-    $cvode_cleanup_memory
-
-    flag = CVodeSetMaxErrTestFails(cvode_mem, $cvode_max_err_test_fails);
-    if (check_flag(&flag, "CVodeSetMaxErrTestFails", 1))
-    $cvode_cleanup_memory
-
-    flag = CVodeSetMaxConvFails(cvode_mem, $cvode_max_conv_fails);
-    if (check_flag(&flag, "CVodeSetMaxConvFails", 1))
-    $cvode_cleanup_memory
-
-    flag = CVodeSetMaxStep(cvode_mem, $cvode_max_step);
-    if (check_flag(&flag, "CVodeSetMaxStep", 1))
-    $cvode_cleanup_memory
-
-    printf("%.16e\\t",timepoints[0]);
-    for(j=0;j<__N_SPECIES__;j++)
-    {
-        printf("%.16e\\t",NV_Ith_S(species,j));
-    }
-    printf("\\n");
-    /* integrate to each timepoint */
-    for ( i=1;  i < n_timepoints;  i++ )
-    {
-        flag = CVode(cvode_mem, timepoints[i], species, &time, CV_NORMAL);
-        if (check_flag(&flag, "CVode", 1))
+        for(j=0;j<nspecies+1;j++)
         {
-            N_VDestroy_Serial(expressions);
-            N_VDestroy_Serial(observables);           
-            N_VDestroy_Serial(ratelaws);
-            N_VDestroy_Serial(species);
-            CVodeFree(&cvode_mem);
-            return 1;
-        }  
-        printf("%.16e\\t",timepoints[i]);
-        for(j=0;j<__N_SPECIES__;j++)
-        {
-            printf("%.16e\\t",NV_Ith_S(species,j));
+            printf("%.16e\t",species_out[counter]);
+            counter++;
         }
-    	printf("\\n");
+        printf("\n");
     }
- 
-    /* Free vectors */
-    N_VDestroy_Serial(expressions);
-    N_VDestroy_Serial(observables);  
-    N_VDestroy_Serial(ratelaws);        
-    N_VDestroy_Serial(species);
-
-    /* Free integrator memory */
-    CVodeFree(&cvode_mem);
-
     return 0;
 }
 
