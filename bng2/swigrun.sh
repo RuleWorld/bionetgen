effa4bec3ff6b0234b9b74708a5b33ddf3d66be2
Sanjana Gupta <sag134@pitt.edu>
2018-03-12 22:06:54 -0400

    export library path

--- bng2/swigrun.sh
+++ bng2/swigrun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./lib/
 swig -python example.i
 gcc -c -fPIC `pkg-config --cflags --libs python`  example.c example_wrap.c
  ld -shared example.o example_wrap.o  /usr/local/MATLAB/R2017a/bin/glnxa64/libsundials_cvode.so.1 /usr/local/MATLAB/R2017a/bin/glnxa64/libsundials_nvecserial.so.0 -lm -o _example.so
