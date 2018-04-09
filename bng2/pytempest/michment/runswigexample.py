385b718bae2b0d42af1e175e44bfd8d78247e681
Sanjana Gupta <sag134@pitt.edu>
2018-04-09 16:25:49 -0400

    example of calling c functions from python via swig

---
+++ bng2/pytempest/michment/runswigexample.py
@@ -0,0 +1,17 @@
+import MM
+from MM import *
+a = new_doubleArray(5)
+params =  [-2.77, -1, -2, 6.02, 602]
+for i in range(0,5):
+	doubleArray_setitem(a,i,params[i])
+c = bng_protocol(a)
+d = doubleArrayClass_frompointer(c)
+for i in range(0,55):
+	print d[i]
+	if i+1%5==0:
+		print "\n"
+
+
+
+
+
